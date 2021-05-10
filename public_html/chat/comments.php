f8510a55e0c29b1078836a26f02383116b8370a5
Sasha Broslavskiy <sasha.broslavskiy@gmail.com>
2021-05-11 02:12:45 +0300

    zzz

--- public_html/chat/comments.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . "/../../vendor/autoload.php";
-require_once __DIR__ . "/inc_common.php";
-
-include($engine_path . "users_get_list.php");
-
-include($file_path . "designes/" . $design . "/comments.php");
